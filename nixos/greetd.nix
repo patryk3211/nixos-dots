91d0fc44c41a610af4d04f286031e2b72427a9ce
patryk3211 <patrykmierzy@gmail.com>
2023-08-04 20:21:58 +0200

    Quick fix since regreet seems to fail me

--- nixos/greetd.nix
+++ nixos/greetd.nix
@@ -1,21 +1,23 @@
 { config, pkgs, lib, ... }:
 with lib;
 let
-  swayStartup = pkgs.writeText "sway-startup" ''
-    ${concatStringsSep "\n" (attrsets.mapAttrsToList (n: v: "export ${n}=${v}") config.profile.wm.env)}
-    ${pkgs.sway}/bin/sway --config ${swayConf} --unsupported-gpu
-  '';
-  swayConf = pkgs.writeText "sway.conf" ''
-    ${concatStringsSep "\n" (attrsets.mapAttrsToList (n: v: 
-      ''output ${n} {
-        mode ${v.resolution}@${v.framerate}Hz
-        scale ${v.scale}
-      }''
+  /*hyprConf = pkgs.writeText "hyprland.conf" ''
+    ${concatStringsSep "\n" (attrsets.mapAttrsToList (n: v:
+      "monitor = ${n}, ${v.resolution}@${v.framerate}, ${v.position}, ${v.scale}"
     ) config.profile.wm.monitors)}
 
-    exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
-    exec "${config.programs.regreet.package}/bin/regreet; ${pkgs.sway}/bin/swaymsg exit"
+    ${concatStringsSep "\n" (
+      attrsets.mapAttrsToList (n: v:
+        "env = ${n},${v}"
+      ) config.profile.wm.env
+    )}
+
+    exec-once = dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+    exec-once = ${pkgs.greetd.}/bin/gtkgreet; ${pkgs.hyprland}/bin/hyprctl dispatch exit
   '';
+  greetStartup = pkgs.writeText "greet-startup" ''
+    ${pkgs.hyprland}/bin/Hyprland -c ${hyprConf}
+  '';*/
 in {
   environment.systemPackages = [
     config.theme.cursor.package
@@ -29,34 +31,39 @@ in {
     group = "greeter";
   };
 
-  programs.regreet = {
+  /*programs.regreet = {
     enable = true;
     settings = {
-      env = config.profile.wm.env // {
-        XDG_CURRENT_DESKTOP = "sway";
-        XDG_SESSION_DESKTOP = "sway";
-      };
-      GTK = {
-        application_prefer_dark_theme = true;
-        cursor_theme_name = config.theme.cursor.name;
-        icon_theme_name = config.theme.icon.name;
-        theme_name = config.theme.gtk.name;
-        font = "Iosevka 16";
-      };
+     background = {
+       path = "/usr/share/backgrounds/login.png";
+       fit = "Contain";
+     };
+     GTK = {
+       application_prefer_dark_theme = true;
+       cursor_theme_name = config.theme.cursor.name;
+       icon_theme_name = config.theme.icon.name;
+       theme_name = config.theme.gtk.name;
+       font = "Iosevka 16";
+     };
     };
-  };
+  };*/
 
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.bash}/bin/bash ${swayStartup}";
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd 'zsh -c Hyprland'";
+        #command = "${pkgs.bash}/bin/bash ${greetStartup}";
         user = "greeter";
       };
     };
+    restart = false;
   };
 
-  environment.etc."greetd/environments".text = ''
-    Hyprland
+  environment.etc."greetd/sessions/Hyprland.desktop".text = ''
+    [Desktop Entry]
+    Name=Hyprland
+    Exec=${pkgs.bash} -c 'Hyprland'
+    Type=Application
   '';
 }
